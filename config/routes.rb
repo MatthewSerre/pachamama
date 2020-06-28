f8705acdd3fd6f0e15ae0822c08b548d965a4683
Matthew Serre <matthew_serre+github@outlook.com>
2020-06-28 14:22:02 -0400

    Create Product scaffold for basic CRUD operations through API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,1 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+Rails.application.routes.draw do namespace :api do  resources :products  end end# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlend
